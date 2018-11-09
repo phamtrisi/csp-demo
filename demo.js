ac05a3dc1097f02f08e546c7633f1ccba2a00b4a
Si Pham <spham@fb.com>
2018-11-09 14:00:19 -0800

    more code

--- demo.js
+++ demo.js
@@ -1,1 +1,4 @@
-alert("you've been pwned");
+var hacked = document.createElement('div');
+hacked.style = "position: fixed; width: 80%; height: 250px; border: 4px solid red; color: red; font-size: 40px; z-index: 1; background: #fff; margin: 40px; text-align: center; line-height: 250px;";
+hacked.appendChild(document.createTextNode("You've been PWNED"));
+document.body.prepend(hacked);
